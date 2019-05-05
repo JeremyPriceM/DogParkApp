25989c32de668319187750e21a760cf48e2bd185
Jeremy Price <JeremyPriceM@gmail.com>
2019-05-05 17:48:52 -0500

    test

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 'use strict';
-require('dotenv').config();
+require('dotenv').config({path: '.env'});
 const express = require('express');
 var methodOverride = require('method-override');
 const app = express();
