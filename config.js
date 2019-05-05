c9c410f77075d03cb2e7247035e55c2e4ebf7504
Jeremy Price <JeremyPriceM@gmail.com>
2019-05-05 17:12:21 -0500

    Fix URL Parser

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 exports.DATABASE_URL = 
     process.env.DATABASE_URL ||
     global.DATABASE_URL ||
-'mongodb+srv://my-new-user:Coolguy32@my-first-mlab-db-vmc3g.mongodb.net/dogparksapp?retryWrites=true',{useNewUrlParser: true};
+'mongodb+srv://my-new-user:Coolguy32@my-first-mlab-db-vmc3g.mongodb.net/dogparksapp?retryWrites=true';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 
 'mongodb://localhost/test-dogparksapp';
 exports.PORT = process.env.PORT || 8080;       
--- server.js
+++ server.js
@@ -29,7 +29,7 @@ let server;
 
 function runServer(databaseUrl, port=PORT) {
     return new Promise((resolve, reject) => {
-        mongoose.connect(databaseUrl, err => {
+        mongoose.connect(databaseUrl, {useNewUrlParser: true}, err => {
             if (err) {
                 return reject(err);
             }
