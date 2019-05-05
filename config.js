f8a012b9bea1b238b7d77083146fd0a2322d75f1
Jeremy Price <JeremyPriceM@gmail.com>
2019-05-05 17:29:25 -0500

    test

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-exports.DATABASE_URL = 
-    process.env.DATABASE_URL ||
-    global.DATABASE_URL ||
+'use strict'
+
+exports.DATABASE_URL = process.env.DATABASE_URL ||
 "mongodb+srv://my-new-user:Coolguy32@my-first-mlab-db-vmc3g.mongodb.net/dogparksapp?retryWrites=true";
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 
 'mongodb://localhost/test-dogparksapp';
