768ff2aa8c5cc2b5fe8954be1f02928938501f73
Jeremy Price <JeremyPriceM@gmail.com>
2019-05-06 20:20:46 -0500

    test

--- config.js
+++ config.js
@@ -1,10 +1,7 @@
 'use strict'
 
-require('dotenv').config({path: '.env'});
-
-
 exports.DATABASE_URL = process.env.DATABASE_URL ||
-"mongodb+srv://my-new-user:Coolguy32@my-first-mlab-db-vmc3g.mongodb.net/dogparksapp?retryWrites=true?";
+String("mongodb+srv://my-new-user:Coolguy32@my-first-mlab-db-vmc3g.mongodb.net/dogparksapp?retryWrites=true?");
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 
 'mongodb://localhost/test-dogparksapp';
 exports.PORT = process.env.PORT || 8080;       
