a598e28191cd4f181414e9d5e43bb762ff96679e
Jeremy Price <JeremyPriceM@gmail.com>
2019-05-06 19:51:08 -0500

    test

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict'
 
 exports.DATABASE_URL = process.env.DATABASE_URL ||
-MongoClient.connect("mongodb+srv://my-new-user:Coolguy32@my-first-mlab-db-vmc3g.mongodb.net/dogparksapp?retryWrites=true?", {useNewUrlParser:true});
+("mongodb+srv://my-new-user:Coolguy32@my-first-mlab-db-vmc3g.mongodb.net/dogparksapp?retryWrites=true?", {useNewUrlParser:true});
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 
 'mongodb://localhost/test-dogparksapp';
 exports.PORT = process.env.PORT || 8080;       
